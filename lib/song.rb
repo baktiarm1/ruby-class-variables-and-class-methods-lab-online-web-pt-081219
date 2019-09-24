7894576d2047107684b4ec2ef405483d8ca1754d
Baktiar Miah <baktiar.m@gmail.com>
2019-09-24 23:13:51 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song 
+
+  attr_accessor :name, :artist, :genre
+  @@genres = []
+  @@artists = []
+  @@count = 0 
+
+  def initialize(name, aritist, genre)
+    @@count+=1
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    
+  end 
+  
+  def count 
+    @@count = count
+  end 
+  
+  def genres 
+    @genres=genres
+    @@genres << genres
+    @@genres.uniq
+  end 
+  
+  def genre_count 
+  end 
+  def artist_count
+  end 
+  
+
+end 
